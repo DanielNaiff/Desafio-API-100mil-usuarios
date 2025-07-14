770c27d3ed0ea1517d66af8686f78fe1549a3a82
Daniel Naiff <danielnaiff344@gmail.com>
2025-07-14 20:23:22 -0300

    readme

---
+++ deasfioAPI/src/main/java/spring/boot/deasfioAPI/DeasfioApiApplication.java
@@ -0,0 +1,13 @@
+package spring.boot.deasfioAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeasfioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeasfioApiApplication.class, args);
+	}
+
+}
---
+++ deasfioAPI/src/test/java/spring/boot/deasfioAPI/DeasfioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.boot.deasfioAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeasfioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
